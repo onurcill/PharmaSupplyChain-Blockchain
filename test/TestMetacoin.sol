6ad62304494cda281ff9f6bfd215948bc2685deb
onurcill <onurcil08@gmail.com>
2022-06-20 23:42:08 +0300

    feat(contracts): added new contracts and frontend template

--- test/TestMetacoin.sol
+++ test/TestMetacoin.sol
@@ -1,31 +1,25 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.22 <0.7.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/MetaCoin.sol";
 
 contract TestMetacoin {
-    function testInitialBalanceUsingDeployedContract() public {
-        MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-        uint expected = 10000;
+  function testInitialBalanceUsingDeployedContract() public {
+    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-        Assert.equal(
-            meta.getBalance(msg.sender),
-            expected,
-            "Owner should have 10000 MetaCoin initially"
-        );
-    }
+    uint expected = 10000;
 
-    function testInitialBalanceWithNewMetaCoin() public {
-        MetaCoin meta = new MetaCoin();
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+  }
 
-        uint expected = 10000;
+  function testInitialBalanceWithNewMetaCoin() public {
+    MetaCoin meta = new MetaCoin();
 
-        Assert.equal(
-            meta.getBalance(address(this)),
-            expected,
-            "Owner should have 10000 MetaCoin initially"
-        );
-    }
+    uint expected = 10000;
+
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+  }
+
 }
